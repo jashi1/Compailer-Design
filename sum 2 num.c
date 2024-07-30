9266ade0b16ec5d39deed5c6a01b404f033c2ddb
jashi1 <124244883+jashi1@users.noreply.github.com>
2024-07-30 21:21:57 +0530

    Add files via upload

---
+++ sum 2 num.c
@@ -0,0 +1,12 @@
+#inclde<stdio.h>
+int main()
+{
+int num1,num2,sum;
+printf("Enter the num1:");
+printf("Enter the num2:");
+scanf("%d",&num1);
+scanf("%d",&num1);
+sum=num1+num2;
+printf("%d+%d",num1,num2,sum);
+return 0;
+}
---
+++ sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+I
+
+int main()
+
+int number 1, number2, sum;
+
+printf("Enter two integers: ");
+
+scanf("%d %d", &number 1, &number2);
+
+sum = number 1 + number 2
+
+printf("dod-%d", number 1, number2, sum),
+
+return 0
